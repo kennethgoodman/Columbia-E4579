7698c23ac15e80d06c6e5ddeeb834f61d7e47e17
kennethgoodman <kennethsgoodman@yahoo.com>
2022-07-01 11:26:11 -0500

    adding logger to app

--- project/__init__.py
+++ project/__init__.py
@@ -1,16 +1,29 @@
 # init.py
-
+import logging
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager 
+from flask_login import LoginManager
+from project.logging_utils.config import configure_logging
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
 
 def create_app():
+    if not os.path.isdir('logs_folder'):
+        os.mkdir('logs_folder')
+    error_logger = configure_logging('logs_folder/error.log', logging.WARN, 'error_log')
+    info_logger = configure_logging('logs_folder/info.log', logging.INFO, 'info_log')
+    debug_logger = configure_logging('logs_folder/debug.log', logging.DEBUG, 'debug_log')
+    loggers = [error_logger, info_logger, debug_logger]
     # to load the flask data, set static folder to build
     app = Flask(__name__, static_folder='project/frontend/build')
+
+    # add them all together
+    app.logger.handlers = list(sum(map(lambda x: x.handlers, loggers), []))
+    # take the minimum level
+    app.logger.setLevel(min(map(lambda x: x.level, loggers)))
 
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
