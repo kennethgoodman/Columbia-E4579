28144bba8ad57f4501b9c0e6461860098a909131
kennethgoodman <kennethsgoodman@yahoo.com>
2022-06-30 19:48:55 -0500

    adding base files

---
+++ project/models.py
@@ -0,0 +1,9 @@
+# models.py
+
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
