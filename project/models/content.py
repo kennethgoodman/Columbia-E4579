e7b8490efb320164f36539cb16a923bd76d9c2e1
kennethgoodman <kennethsgoodman@yahoo.com>
2022-07-03 00:23:29 -0500

    adding s3 bucket

--- project/models/content.py
+++ project/models/content.py
@@ -18,8 +18,9 @@ class Content(db.Model):
     __tablename__ = "content"
     id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     media_type = db.Column(SqlEnum(MediaType))
-    s3_id = db.Column(db.String(100), nullable=True)  # might be only text, if media_type = Text
-    text = db.Column(db.String(1000))  # text on the post
-    author_id = db.Column(db.Integer, ForeignKey("user.id"))
+    s3_bucket = db.Column(db.String(200), nullable=True)
+    s3_id = db.Column(db.String(200), nullable=True)  # might be only text, if media_type = Text
+    text = db.Column(db.String(1000), nullable=True)  # text on the post
+    author_id = db.Column(db.Integer, ForeignKey("user.id"), nullable=False)
     # all engagements on the content
     content_engagements = relationship("Engagement")  # one piece of content with many engagements
