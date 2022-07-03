3b0fb4d5a9fe07706d9c5bb294afdf256ae47f86
kennethgoodman <kennethsgoodman@yahoo.com>
2022-07-02 20:38:51 -0500

    some whitespace

--- project/models/user.py
+++ project/models/user.py
@@ -3,7 +3,8 @@
 from flask_login import UserMixin
 from project import db
 
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
