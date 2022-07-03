b9da8a20b980dae364ebb14e8925d12e27f1018b
kennethgoodman <kennethsgoodman@yahoo.com>
2022-07-02 22:55:12 -0500

    Adding random photo url for testing

--- project/backend_routes/main.py
+++ project/backend_routes/main.py
@@ -12,6 +12,22 @@ def profile():
     return render_template('profile.html', username=current_user.username)
 
 
+@main.route('/random_photo')
+def random_photo():
+    image_url = "https://columbia-fall2022-e4579-images.s3.amazonaws.com/Images/n02085620-Chihuahua/n02085620_10074.jpg"
+    return f"""
+        <!DOCTYPE html>
+        <html>
+        <body>
+        
+        <h1>The img element</h1>
+        
+        <img src="{image_url}">
+        
+        </body>
+        </html>
+    """
+
 # Serve React App
 @main.route('/')
 def index():
