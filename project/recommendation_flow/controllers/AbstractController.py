9bf51a5c46339d66d7e118d105973d9308f54b4d
kennethgoodman <kennethsgoodman@yahoo.com>
2022-07-02 20:39:53 -0500

    adding controllers

---
+++ project/recommendation_flow/controllers/AbstractController.py
@@ -0,0 +1,2 @@
+class AbstractController:
+    pass
---
+++ project/recommendation_flow/controllers/RandomController.py
@@ -0,0 +1,5 @@
+from project.recommendation_flow.controllers.AbstractController import AbstractController
+
+
+class RandomController(AbstractController):
+    pass
