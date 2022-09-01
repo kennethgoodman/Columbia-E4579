2930e5abde96f9537216769089a94b89c1ce51cc
kennethgoodman <kennethsgoodman@yahoo.com>
2022-08-31 20:29:11 -0500

    Adding intersection observer

--- project/data_models/engagement.py
+++ project/data_models/engagement.py
@@ -9,15 +9,14 @@ class EngagementType(Enum):
     Like = 1
     Comment = 2
     VideoWatch = 3  # more than 100ms
+    ImageView = 4   # more than 75% of the photo for > 200ms
+    MillisecondsEngagedWith = 5
 
 
 class Engagement(db.Model):
-    __table_args__ = (
-        # this can be db.PrimaryKeyConstraint if you want it to be a primary key
-        db.UniqueConstraint('user_id', 'content_id', 'engagement_type'),
-    )
     __tablename__ = "engagement"
     id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     user_id = db.Column(db.Integer, ForeignKey('user.id'))  # user that engaged
     content_id = db.Column(db.Integer, ForeignKey('content.id'))  # content they engaged with
     engagement_type = db.Column(SqlEnum(EngagementType))  # how they engaged
+    # engagement_value = db.Column(db.Integer, nullable=True)  # the value of the engagement_type
