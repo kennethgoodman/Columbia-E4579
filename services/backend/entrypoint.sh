f25745a4eba229440b11bf6af5fe382ff9c44c1c
kennethgoodman <kennethsgoodman@yahoo.com>
2022-09-12 22:36:33 -0500

    ensuring running seed db on startup

--- services/backend/entrypoint.sh
+++ services/backend/entrypoint.sh
@@ -8,6 +8,10 @@ done
 
 echo "mysql started"
 
-python manage.py run -h 0.0.0.0
+echo "recreating db"
 python manage.py recreate_db
+echo "db created"
 python manage.py seed_db
+echo "db seeded"
+
+python manage.py run -h 0.0.0.0
