aa535120f254dab244dcc08a09c15befc238899c
kennethgoodman <kennethsgoodman@yahoo.com>
2022-11-02 23:14:24 -0500

    removing with sesion

--- services/backend/src/tests/conftest.py
+++ services/backend/src/tests/conftest.py
@@ -23,9 +23,8 @@ def test_database():
 def add_user():
     def _add_user(username, password):
         user = User(username=username, password=password)
-        with db.session() as session:
-            session.add(user)
-            session.commit()
+        db.session.add(user)
+        db.session.commit()
         return user
 
     return _add_user
