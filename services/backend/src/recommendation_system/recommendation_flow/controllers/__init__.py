da261afb7e6bfa98d4551cede9bfa8465fec48b0
Monica Guntur <75153702+monicaguntur@users.noreply.github.com>
2022-11-25 16:47:32 -0500

    Group Delta Recommender Flow

--- services/backend/src/recommendation_system/recommendation_flow/controllers/__init__.py
+++ services/backend/src/recommendation_system/recommendation_flow/controllers/__init__.py
@@ -19,3 +19,7 @@ from src.recommendation_system.recommendation_flow.controllers.BetaController im
 from src.recommendation_system.recommendation_flow.controllers.CharlieController import (
     CharlieController,
 )
+
+from src.recommendation_system.recommendation_flow.controllers.DeltaController import (
+    DeltaController
+)
