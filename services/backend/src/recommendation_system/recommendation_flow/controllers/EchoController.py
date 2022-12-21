aba56e9a2d3587a999130273ee7c152642385daf
Phachara Laohrenu <thepoch@phacharas-air.lan>
2022-12-20 23:20:32 -0500

    revert change on example

---
+++ services/backend/src/recommendation_system/recommendation_flow/controllers/EchoController.py
@@ -0,0 +1,34 @@
+from src.recommendation_system.recommendation_flow.controllers.AbstractController import (
+    AbstractController,
+)
+
+from src.recommendation_system.recommendation_flow.candidate_generators.EchoGenerator import (
+    EchoGenerator,
+)
+from src.recommendation_system.recommendation_flow.filtering.EchoFilter import (
+    EchoFilter,
+)
+from src.recommendation_system.recommendation_flow.model_prediction.EchoModel import (
+    EchoModel,
+)
+from src.recommendation_system.recommendation_flow.ranking.EchoRanker import (
+    EchoRanker,
+)
+
+
+class EchoController(AbstractController):
+
+    def __init__(self):
+        self.filter = EchoFilter()
+        self.candidate_generator = EchoGenerator()
+        self.predictor = EchoModel()
+        self.ranker = EchoRanker()
+
+    def get_content_ids(self, user_id, limit=None, offset=None, seed=None, starting_point=None):
+        user_id = user_id % 60  # For test on locally, the user_id on local webapp can exceed the actual number (59)
+        candidates = self.candidate_generator.get_content_ids(user_id, limit=1000)
+        filtered = self.filter.filter_ids(user_id, candidates)
+        predictions = self.predictor.predict_probabilities(user_id, filtered)
+        recs = self.ranker.rank_ids(predictions, limit, seed, starting_point)
+
+        return recs
--- services/backend/src/recommendation_system/recommendation_flow/controllers/ExampleController.py
+++ services/backend/src/recommendation_system/recommendation_flow/controllers/ExampleController.py
@@ -1,34 +1,41 @@
+from src.recommendation_system.recommendation_flow.candidate_generators.ExampleGenerator import (
+    ExampleGenerator,
+)
 from src.recommendation_system.recommendation_flow.controllers.AbstractController import (
     AbstractController,
 )
-
-from src.recommendation_system.recommendation_flow.candidate_generators.EchoGenerator import (
-    EchoGenerator,
+from src.recommendation_system.recommendation_flow.filtering.ExampleFilter import (
+    ExampleFilter,
 )
-from src.recommendation_system.recommendation_flow.filtering.EchoFilter import (
-    EchoFilter,
+from src.recommendation_system.recommendation_flow.model_prediction.ExampleModel import (
+    ExampleModel,
 )
-from src.recommendation_system.recommendation_flow.model_prediction.EchoModel import (
-    EchoModel,
-)
-from src.recommendation_system.recommendation_flow.ranking.EchoRanker import (
-    EchoRanker,
+from src.recommendation_system.recommendation_flow.ranking.RandomRanker import (
+    RandomRanker,
 )
 
 
 class ExampleController(AbstractController):
-
-    def __init__(self):
-        self.filter = EchoFilter()
-        self.candidate_generator = EchoGenerator()
-        self.predictor = EchoModel()
-        self.ranker = EchoRanker()
-
-    def get_content_ids(self, user_id, limit=None, offset=None, seed=None, starting_point=None):
-        user_id = user_id % 60  # For test on locally, the user_id on local webapp can exceed the actual number (59)
-        candidates = self.candidate_generator.get_content_ids(user_id, limit=1000)
-        filtered = self.filter.filter_ids(user_id, candidates)
-        predictions = self.predictor.predict_probabilities(user_id, filtered)
-        recs = self.ranker.rank_ids(predictions, limit, seed, starting_point)
-
-        return recs
+    def get_content_ids(self, user_id, limit, offset, seed, starting_point):
+        candidates_limit = (
+            limit * 10 * 10
+        )  # 10% gets filtered out and take top 10% of rank
+        candidates, scores = ExampleGenerator().get_content_ids(
+            user_id, candidates_limit, offset, seed, starting_point
+        )
+        filtered_candidates = ExampleFilter().filter_ids(
+            candidates, seed, starting_point
+        )
+        predictions = ExampleModel().predict_probabilities(
+            filtered_candidates,
+            user_id,
+            seed=seed,
+            scores={
+                content_id: {"score": score}
+                for content_id, score in zip(candidates, scores)
+            }
+            if scores is not None
+            else {},
+        )
+        rank = RandomRanker().rank_ids(limit, predictions, seed, starting_point)
+        return rank
