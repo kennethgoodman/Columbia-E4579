0a171ba2dcb98a15da6e3cd0dc58a8b3063865e5
kennethgoodman <kennethsgoodman@yahoo.com>
2022-12-21 19:40:19 -0600

    fixing echo gen

--- services/backend/src/recommendation_system/recommendation_flow/candidate_generators/EchoGenerator.py
+++ services/backend/src/recommendation_system/recommendation_flow/candidate_generators/EchoGenerator.py
@@ -2,12 +2,13 @@ import json
 from .AbstractGenerator import AbstractGenerator
 
 cb = open('src/echo_space/output/cg_cb_recs.json')
-recs_cb = json.load(cb)[str(user_id)]
+recs_loaded = json.load(cb)
 cf = open('src/echo_space/output/cg_cf_recs.json')
-recs_cf = json.load(cf)[str(user_id)]
+recs_loaded = json.load(cf)
 
 class EchoGenerator(AbstractGenerator):
     def get_content_ids(self, user_id, limit=None, offset=None, seed=None, starting_point=None):
+        recs_cb, recs_cf = recs_loaded[str(user_id)], recs_loaded[str(user_id)]
         if not limit:
             return recs_cf + recs_cb
         ratio = len(recs_cf) / (len(recs_cf) + len(recs_cb))
