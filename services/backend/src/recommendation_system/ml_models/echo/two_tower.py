2d137cf660caebeb364f4da2ceccb4fbe63b302d
Jingyan <js5544@columbia.edu>
2023-10-10 15:03:10 -0400

    0

--- services/backend/src/recommendation_system/ml_models/echo/two_tower.py
+++ services/backend/src/recommendation_system/ml_models/echo/two_tower.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 import warnings
 warnings.filterwarnings("ignore")
+import pickle
 
 # Set up basic logging configuration
 logging.basicConfig(level=logging.ERROR)
@@ -17,6 +18,9 @@ TOP_SOURCES = 30
 TOP_SEEDS = 14
 TOP_CONTENT = 251
 PROMPT_EMBEDDING_LENGTH = 512
+with open('/usr/src/app/src/recommendation_system/ml_models/echo/top_n_content.pkl','rb') as file:
+  top_n_content = pickle.load(file)
+
 
 # Two Tower PyTorch Model
 class TwoTowerModel(nn.Module):
@@ -48,34 +52,13 @@ class DummyTwoTowerModel(nn.Module):
         # Return dummy embeddings of shape (user_tensor length, 64)
         return torch.randn((len(user_tensor), 64))
 
-def preprocessing(df):
-    top_artist_styles = df['artist_style'].value_counts().nlargest(TOP_ARTIST_STYLES).index.tolist()
-    top_sources = df['source'].value_counts().nlargest(TOP_SOURCES).index.tolist()
-    top_seeds = df['seed'].value_counts().nlargest(TOP_SEEDS).index.tolist()
-
-    # Replace less frequent artist styles, sources, and seeds with 'other'
-    df['artist_style'] = df['artist_style'].apply(lambda x: x if x in top_artist_styles else 'other')
-    df['source'] = df['source'].apply(lambda x: x if x in top_sources else 'other')
-    df['seed'] = df['seed'].apply(lambda x: str(x) if x in top_seeds else 'other')
-
-    # One-hot encode categorical features
-    encoder = OneHotEncoder()
-    content_onehot = encoder.fit_transform(df[['artist_style', 'model_version', 'seed', 'source']])
-    content_onehot_df = pd.DataFrame(content_onehot.toarray(), columns=encoder.get_feature_names_out(['artist_style', 'model_version', 'seed', 'source']))
-    df = pd.concat([df, content_onehot_df], axis=1)
-
-    # Normalizing linear features
-    scaler = StandardScaler()
-    df[['guidance_scale', 'num_inference_steps']] = scaler.fit_transform(df[['guidance_scale', 'num_inference_steps']])
-
-    # Compute top N content pieces based on engagement_value
-    from collections import defaultdict
-    top_n_content = df.groupby('content_id')['engagement_value'].count().nlargest(TOP_CONTENT).index.tolist()
+def user_preprocessing(df):
+    df['engagement_type'] = df['engagement_type'].apply(lambda x: x.name)    
     user_vector_dict = defaultdict(lambda: {
-        'millisecond_engaged_vector': np.zeros(len(top_n_content)),
-        'like_vector': np.zeros(len(top_n_content)),
-        'dislike_vector': np.zeros(len(top_n_content))
-    })
+    'millisecond_engaged_vector': np.zeros(len(top_n_content)),
+    'like_vector': np.zeros(len(top_n_content)),
+    'dislike_vector': np.zeros(len(top_n_content))
+})
 
     # Initialize vectors for each user
     def aggregate_engagement(group):
@@ -91,25 +74,23 @@ def preprocessing(df):
             'likes_count': likes_count,
             'dislikes_count': dislikes_count
         })
+    for user_id in df['user_id'].unique():
+        user_vector_dict[user_id]['millisecond_engaged_vector']=np.zeros(len(top_n_content))
+        user_vector_dict[user_id]['like_vector']=np.zeros(len(top_n_content))
+        user_vector_dict[user_id]['dislike_vector']=np.zeros(len(top_n_content))
+    if len(df[df['content_id'].isin(top_n_content)]) != 0:
+        engagement_aggregate = df[df['content_id'].isin(top_n_content)].groupby(['user_id', 'content_id']).apply(aggregate_engagement).reset_index()
+        for _, row in engagement_aggregate.iterrows():
+            user_id = row['user_id']
+            content_id = row['content_id']
+            idx = top_n_content.index(content_id)
 
-    # Group by user_id and content_id, then apply the function
-    engagement_aggregate = df[df['content_id'].isin(top_n_content)].groupby(['user_id', 'content_id']).apply(aggregate_engagement).reset_index()
-
-    # Now, populate your user_vector_dict
-    for _, row in engagement_aggregate.iterrows():
-        user_id = row['user_id']
-        content_id = row['content_id']
-        idx = top_n_content.index(content_id)
-
-        user_vector_dict[user_id]['millisecond_engaged_vector'][idx] = row['millisecond_engagement_sum']
-        user_vector_dict[user_id]['like_vector'][idx] = row['likes_count']
-        user_vector_dict[user_id]['dislike_vector'][idx] = row['dislikes_count']
+            user_vector_dict[user_id]['millisecond_engaged_vector'][idx] = row['millisecond_engagement_sum']
+            user_vector_dict[user_id]['like_vector'][idx] = row['likes_count']
+            user_vector_dict[user_id]['dislike_vector'][idx] = row['dislikes_count']
 
     # Convert to DataFrame
     user_vector_df = pd.DataFrame.from_dict(user_vector_dict, orient='index')
-    del user_vector_dict
-
-    # Unpack vector columns into individual columns
     millisecond_columns = [f"ms_engaged_{i}" for i in range(TOP_CONTENT)]
     like_columns = [f"like_vector_{i}" for i in range(TOP_CONTENT)]
     dislike_columns = [f"dislike_vector_{i}" for i in range(TOP_CONTENT)]
@@ -117,7 +98,6 @@ def preprocessing(df):
     user_vector_df[millisecond_columns] = pd.DataFrame(user_vector_df['millisecond_engaged_vector'].tolist(), index=user_vector_df.index)
     user_vector_df[like_columns] = pd.DataFrame(user_vector_df['like_vector'].tolist(), index=user_vector_df.index)
     user_vector_df[dislike_columns] = pd.DataFrame(user_vector_df['dislike_vector'].tolist(), index=user_vector_df.index)
-
     # Drop the original vector columns
     user_vector_df.drop(['millisecond_engaged_vector', 'like_vector', 'dislike_vector'], axis=1, inplace=True)
 
@@ -126,31 +106,118 @@ def preprocessing(df):
         user_vector_df.reset_index().rename(columns={'index': 'user_id'}),
         on='user_id'
     )
-    del user_vector_df
 
-    # Unpack prompt embedding
-    prompt_columns = [f"prompt_embedding_{i}" for i in range(PROMPT_EMBEDDING_LENGTH)]
-    df[prompt_columns] = pd.DataFrame(df['prompt_embedding'].tolist(), index=df.index)
-    df = df.drop('prompt_embedding', axis=1)
-
-    # Create overall features
     user_columns = (
+        ['user_id'] + 
         [f'ms_engaged_{i}' for i in range(TOP_CONTENT)] +
         [f'like_vector_{i}' for i in range(TOP_CONTENT)] +
         [f'dislike_vector_{i}' for i in range(TOP_CONTENT)]
     )
     user_features = df[user_columns]
+    return user_features
+# def preprocessing(df):
+#     top_artist_styles = df['artist_style'].value_counts().nlargest(TOP_ARTIST_STYLES).index.tolist()
+#     top_sources = df['source'].value_counts().nlargest(TOP_SOURCES).index.tolist()
+#     top_seeds = df['seed'].value_counts().nlargest(TOP_SEEDS).index.tolist()
 
-    content_columns = (
-        list(filter(lambda x: 'artist_style_' in x, df.columns)) +
-        list(filter(lambda x: 'model_version_' in x, df.columns)) +
-        list(filter(lambda x: 'source_' in x, df.columns)) +
-        list(filter(lambda x: 'seed_' in x, df.columns)) +
-        list(filter(lambda x: 'prompt_embedding_' in x, df.columns)) +
-        ['content_id', 'guidance_scale', 'num_inference_steps']
-    )
-    content_features = df[content_columns]
-    return content_features, user_features
+#     # Replace less frequent artist styles, sources, and seeds with 'other'
+#     df['artist_style'] = df['artist_style'].apply(lambda x: x if x in top_artist_styles else 'other')
+#     df['source'] = df['source'].apply(lambda x: x if x in top_sources else 'other')
+#     df['seed'] = df['seed'].apply(lambda x: str(x) if x in top_seeds else 'other')
+
+#     # One-hot encode categorical features
+#     encoder = OneHotEncoder()
+#     content_onehot = encoder.fit_transform(df[['artist_style', 'model_version', 'seed', 'source']])
+#     content_onehot_df = pd.DataFrame(content_onehot.toarray(), columns=encoder.get_feature_names_out(['artist_style', 'model_version', 'seed', 'source']))
+#     df = pd.concat([df, content_onehot_df], axis=1)
+
+#     # Normalizing linear features
+#     scaler = StandardScaler()
+#     df[['guidance_scale', 'num_inference_steps']] = scaler.fit_transform(df[['guidance_scale', 'num_inference_steps']])
+
+#     # Compute top N content pieces based on engagement_value
+#     from collections import defaultdict
+#     top_n_content = df.groupby('content_id')['engagement_value'].count().nlargest(TOP_CONTENT).index.tolist()
+#     user_vector_dict = defaultdict(lambda: {
+#         'millisecond_engaged_vector': np.zeros(len(top_n_content)),
+#         'like_vector': np.zeros(len(top_n_content)),
+#         'dislike_vector': np.zeros(len(top_n_content))
+#     })
+
+#     # Initialize vectors for each user
+#     def aggregate_engagement(group):
+#         # Summing millisecond engagement values
+#         millisecond_engagement_sum = group.loc[group['engagement_type'] != 'Like', 'engagement_value'].sum()
+
+#         # Counting likes and dislikes
+#         likes_count = group.loc[(group['engagement_type'] == 'Like') & (group['engagement_value'] == 1)].shape[0]
+#         dislikes_count = group.loc[(group['engagement_type'] == 'Like') & (group['engagement_value'] == -1)].shape[0]
+
+#         return pd.Series({
+#             'millisecond_engagement_sum': millisecond_engagement_sum,
+#             'likes_count': likes_count,
+#             'dislikes_count': dislikes_count
+#         })
+
+#     # Group by user_id and content_id, then apply the function
+#     engagement_aggregate = df[df['content_id'].isin(top_n_content)].groupby(['user_id', 'content_id']).apply(aggregate_engagement).reset_index()
+
+#     # Now, populate your user_vector_dict
+#     for _, row in engagement_aggregate.iterrows():
+#         user_id = row['user_id']
+#         content_id = row['content_id']
+#         idx = top_n_content.index(content_id)
+
+#         user_vector_dict[user_id]['millisecond_engaged_vector'][idx] = row['millisecond_engagement_sum']
+#         user_vector_dict[user_id]['like_vector'][idx] = row['likes_count']
+#         user_vector_dict[user_id]['dislike_vector'][idx] = row['dislikes_count']
+
+#     # Convert to DataFrame
+#     user_vector_df = pd.DataFrame.from_dict(user_vector_dict, orient='index')
+#     del user_vector_dict
+
+#     # Unpack vector columns into individual columns
+#     millisecond_columns = [f"ms_engaged_{i}" for i in range(TOP_CONTENT)]
+#     like_columns = [f"like_vector_{i}" for i in range(TOP_CONTENT)]
+#     dislike_columns = [f"dislike_vector_{i}" for i in range(TOP_CONTENT)]
+
+#     user_vector_df[millisecond_columns] = pd.DataFrame(user_vector_df['millisecond_engaged_vector'].tolist(), index=user_vector_df.index)
+#     user_vector_df[like_columns] = pd.DataFrame(user_vector_df['like_vector'].tolist(), index=user_vector_df.index)
+#     user_vector_df[dislike_columns] = pd.DataFrame(user_vector_df['dislike_vector'].tolist(), index=user_vector_df.index)
+
+#     # Drop the original vector columns
+#     user_vector_df.drop(['millisecond_engaged_vector', 'like_vector', 'dislike_vector'], axis=1, inplace=True)
+
+#     # Join User Vector To Df
+#     df = df.merge(
+#         user_vector_df.reset_index().rename(columns={'index': 'user_id'}),
+#         on='user_id'
+#     )
+#     del user_vector_df
+
+#     # Unpack prompt embedding
+#     prompt_columns = [f"prompt_embedding_{i}" for i in range(PROMPT_EMBEDDING_LENGTH)]
+#     df[prompt_columns] = pd.DataFrame(df['prompt_embedding'].tolist(), index=df.index)
+#     df = df.drop('prompt_embedding', axis=1)
+
+#     # Create overall features
+#     user_columns = (
+#         [f'ms_engaged_{i}' for i in range(TOP_CONTENT)] +
+#         [f'like_vector_{i}' for i in range(TOP_CONTENT)] +
+#         [f'dislike_vector_{i}' for i in range(TOP_CONTENT)]
+#     )
+#     user_features = df[user_columns]
+
+#     content_columns = (
+#         list(filter(lambda x: 'artist_style_' in x, df.columns)) +
+#         list(filter(lambda x: 'model_version_' in x, df.columns)) +
+#         list(filter(lambda x: 'source_' in x, df.columns)) +
+#         list(filter(lambda x: 'seed_' in x, df.columns)) +
+#         list(filter(lambda x: 'prompt_embedding_' in x, df.columns)) +
+#         ['content_id', 'guidance_scale', 'num_inference_steps']
+#     )
+#     content_features = df[content_columns]
+#     return content_features, user_features
 
 # Functions to convert DataFrame to Tensors
 def df_to_content_tensor(df):
@@ -196,7 +263,7 @@ class ModelWrapper:
 
     def generate_user_embeddings(self, df):
         # user_features = user_preprocessing(df)
-        _, user_features = preprocessing(df)
+        _, user_features = user_preprocessing(df)
         user_tensor = df_to_user_tensor(user_features)
         # user_tensor = df_to_user_tensor(df)
         if len(df["user_id"].unique()) != len(user_tensor):
