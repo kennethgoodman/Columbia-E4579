13de56dd71885650c949cb5db0fe69d939deba7b
Jingyan <js5544@columbia.edu>
2023-10-13 16:31:28 -0400

    0

--- services/backend/src/recommendation_system/recommendation_flow/controllers/fall_2023/EchoController.py
+++ services/backend/src/recommendation_system/recommendation_flow/controllers/fall_2023/EchoController.py
@@ -24,7 +24,7 @@ class EchoController(AbstractController):
     def get_content_ids(self, user_id, limit, offset, seed, starting_point):
         if seed <= 1:  # MySql seeds should be [0, # of rows] not [0, 1]
             seed *= 1000000
-        candidate_limit = set
+        candidate_limit = 500
         candidates, scores = [], []
         for gen in [CollaberativeFilteredSimilarUsersGenerator]: # , CollaberativeFilteredSimilarUsersGenerator, YourChoiceGenerator, TwoTowerANNGenerator
            cur_candidates, cur_scores = gen().get_content_ids(
