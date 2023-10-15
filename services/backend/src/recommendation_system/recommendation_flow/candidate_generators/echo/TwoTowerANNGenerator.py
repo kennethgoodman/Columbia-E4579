58015bb43951d8e1ff042a4bd34d3441443bc971
Jingyan <js5544@columbia.edu>
2023-10-15 11:10:06 -0400

    0

--- services/backend/src/recommendation_system/recommendation_flow/candidate_generators/echo/TwoTowerANNGenerator.py
+++ services/backend/src/recommendation_system/recommendation_flow/candidate_generators/echo/TwoTowerANNGenerator.py
@@ -12,7 +12,7 @@ class TwoTowerANNGenerator(AbstractGenerator):
         else:
             content_ids, scores = get_ANN_recommendations_from_user(user_id, "echo", limit + offset)
         print(len(content_ids))
-        # print(content_ids[offset:])
+        print(content_ids[offset:])
         return content_ids[offset:], scores[offset:]
 
     def _get_name(self):
