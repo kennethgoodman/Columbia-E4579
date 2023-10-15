b5b3f02019f89f60cf75846c146354f64fd33b20
Archit0303 <134573261+Archit0303@users.noreply.github.com>
2023-10-15 19:57:43 -0400

    Add files via upload

---
+++ GolfController.py
@@ -0,0 +1,52 @@
+
+from src.recommendation_system.recommendation_flow.candidate_generators.RandomGenerator import (
+    RandomGenerator,
+)
+from src.recommendation_system.recommendation_flow.controllers.AbstractController import (
+    AbstractController,
+)
+from src.recommendation_system.recommendation_flow.filtering.RandomFilter import (
+    RandomFilter,
+)
+from src.recommendation_system.recommendation_flow.model_prediction.RandomModel import (
+    RandomModel,
+)
+from src.recommendation_system.recommendation_flow.ranking.RandomRanker import (
+    RandomRanker,
+)
+from src.recommendation_system.recommendation_flow.candidate_generators.golf.TwoTowerANNGenerator import TwoTowerANNGenerator
+from src.recommendation_system.recommendation_flow.candidate_generators.golf.CollaberativeFilteredSimilarUsersGenerator import CollaberativeFilteredSimilarUsersGenerator
+from src.recommendation_system.recommendation_flow.candidate_generators.golf.YourChoiceGenerator import YourChoiceGenerator
+
+from src.api.metrics.models import TeamName
+
+class GolfController(AbstractController):
+    def get_content_ids(self, user_id, limit, offset, seed, starting_point):
+        if seed <= 1:  # MySql seeds should be [0, # of rows] not [0, 1]
+            seed *= 1000000
+        candidate_limit = 500
+        candidates, scores = [], []
+        # TwoTowerANNGenerator,CollaberativeFilteredSimilarUsersGenerator,YourChoiceGenerator
+        for gen in [TwoTowerANNGenerator,CollaberativeFilteredSimilarUsersGenerator,YourChoiceGenerator]:
+           cur_candidates, cur_scores = gen().get_content_ids(
+               TeamName.Golf_F2023,
+               user_id, candidate_limit, offset, seed, starting_point
+           )
+           candidates += cur_candidates
+           scores += cur_scores
+        filtered_candidates = RandomFilter().filter_ids(
+            candidates, seed, starting_point
+        )
+        predictions = RandomModel().predict_probabilities(
+            filtered_candidates,
+            user_id,
+            seed=seed,
+            scores={
+                content_id: {"score": score}
+                for content_id, score in zip(candidates, scores)
+            }
+            if scores is not None
+            else {},
+        )
+        rank = RandomRanker().rank_ids(limit, predictions, seed, starting_point)
+        return rank
