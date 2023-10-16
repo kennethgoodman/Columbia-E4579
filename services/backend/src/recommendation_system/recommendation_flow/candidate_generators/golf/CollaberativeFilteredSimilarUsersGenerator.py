af2b87391187e42204f1452f4784609245bfac8f
zwht81 <66426651+zwht81@users.noreply.github.com>
2023-10-15 22:36:07 -0400

    Add files via upload

--- services/backend/src/recommendation_system/recommendation_flow/candidate_generators/golf/CollaberativeFilteredSimilarUsersGenerator.py
+++ services/backend/src/recommendation_system/recommendation_flow/candidate_generators/golf/CollaberativeFilteredSimilarUsersGenerator.py
@@ -4,8 +4,12 @@ from src.data_structures.user_based_recommender.golf.UserBasedRecommender import
 
 
 class CollaberativeFilteredSimilarUsersGenerator(AbstractGenerator):
-    def _get_content_ids(self, _, limit, offset, _seed, starting_point):
-        return [], []
-    
+    def _get_content_ids(self, user_id, limit, offset, seed, starting_point):
+
+        recommender = UserBasedRecommender()
+        content_ids = recommender.recommend_items(user_id, limit)
+
+        return content_ids, []
+
     def _get_name(self):
         return "CollaberativeFilteredSimilarUsersGenerator"
