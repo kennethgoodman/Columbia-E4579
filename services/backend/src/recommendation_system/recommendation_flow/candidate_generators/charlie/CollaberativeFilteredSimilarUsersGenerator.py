715db3432bf3b224dc150b772c2c2206ed53a3fa
hhagy <148026520+hhagy@users.noreply.github.com>
2023-10-15 23:14:19 -0400

    Add files via upload

--- services/backend/src/recommendation_system/recommendation_flow/candidate_generators/charlie/CollaberativeFilteredSimilarUsersGenerator.py
+++ services/backend/src/recommendation_system/recommendation_flow/candidate_generators/charlie/CollaberativeFilteredSimilarUsersGenerator.py
@@ -1,11 +1,30 @@
-
+import pandas as pd
 from src.recommendation_system.recommendation_flow.candidate_generators.AbstractGenerator import AbstractGenerator
 from src.data_structures.user_based_recommender.charlie.UserBasedRecommender import UserBasedRecommender
 
 
 class CollaberativeFilteredSimilarUsersGenerator(AbstractGenerator):
-    def _get_content_ids(self, _, limit, offset, _seed, starting_point):
-        return [], []
-    
+    def _get_content_ids(self, user_id, limit, offset, _seed, starting_point):
+        # Get recommendations based on user-based collaborative filtering
+        ubr = UserBasedRecommender()
+        ubr.compute_similarity()
+        recommended_content = ubr.recommend_items(user_id, 2 * limit)
+        # return [tup[0] for tup in recommended_content], [0] * 2 * limit
+        content_ids = []
+        scores = []
+        index = 0
+        while index < 2 * limit:
+            content_ids.append(recommended_content[index][0])
+            if recommended_content[index][1] == 1:
+                scores.append(1 + 3000 / recommended_content[index + 1][1])
+                index += 2
+            else:
+                scores.append(recommended_content[index][1] / 30001)
+                index += 1
+        temp = pd.Series(scores, index=content_ids).sort_values(ascending=False)
+        content_ids = temp.index.tolist()
+        scores = temp.tolist()
+        return content_ids[:limit], scores[:limit]
+
     def _get_name(self):
         return "CollaberativeFilteredSimilarUsersGenerator"
--- services/backend/src/recommendation_system/recommendation_flow/candidate_generators/charlie/YourChoiceGenerator.py
+++ services/backend/src/recommendation_system/recommendation_flow/candidate_generators/charlie/YourChoiceGenerator.py
@@ -1,9 +1,35 @@
+from sqlalchemy import text
+from sqlalchemy.sql.expression import func
+from src import db
+from src.api.content.models import Content
+from src.data_structures.approximate_nearest_neighbor import ann_with_offset
+from src.api.engagement.models import Engagement, EngagementType
+
 
 from src.recommendation_system.recommendation_flow.candidate_generators.AbstractGenerator import AbstractGenerator
 
 class YourChoiceGenerator(AbstractGenerator):
-    def _get_content_ids(self, _, limit, offset, _seed, starting_point):
-        return [], []
-    
+    def _get_content_ids(self, user_id, limit, offset, seed, starting_point):
+        if starting_point is None:
+            results = (
+                Engagement.query.with_entities(
+                    Engagement.content_id, func.count()
+                )
+                .filter_by(
+                    engagement_type=EngagementType.Like,
+                )
+                .group_by(Engagement.content_id)
+                .order_by(func.count().desc())
+                .limit(limit)
+                .offset(offset)
+                .all()
+            )
+            return list(map(lambda x: x[0], results)), [0]*(len(results))
+        elif starting_point.get("content_id", False):
+            content_ids, scores = ann_with_offset(
+                starting_point["content_id"], 0.9, limit, offset, return_distances=True
+            )
+            return content_ids, scores
+        raise NotImplementedError("Need to provide a key we know about")
     def _get_name(self):
         return "YourChoiceGenerator"
