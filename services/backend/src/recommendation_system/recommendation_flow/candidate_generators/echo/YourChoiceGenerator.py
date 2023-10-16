922c7d19b8b4cc72cf1af67ca89f60b2bfc3a646
JYS99 <95613112+JYS-99@users.noreply.github.com>
2023-10-16 14:53:05 -0400

    Update YourChoiceGenerator.py

--- services/backend/src/recommendation_system/recommendation_flow/candidate_generators/echo/YourChoiceGenerator.py
+++ services/backend/src/recommendation_system/recommendation_flow/candidate_generators/echo/YourChoiceGenerator.py
@@ -1,34 +1,24 @@
-import operator
 from sqlalchemy.sql.expression import func, text
-import datetime
 from src import db
-from src.api.content.models import Content
 from src.api.engagement.models import Engagement, EngagementType
-from src.data_structures.approximate_nearest_neighbor import ann_with_offset
 from src.recommendation_system.recommendation_flow.candidate_generators.AbstractGenerator import AbstractGenerator
 
 class YourChoiceGenerator(AbstractGenerator):
     def _get_content_ids(self, _, limit, offset, _seed, starting_point):
-        if starting_point is None:
-            results = (
-                Engagement.query.with_entities(
-                    Engagement.content_id, func.sum((Engagement.engagement_value) * (315360000 - func.timestampdiff(text("SECOND"), Engagement.created_date, func.now()))).label('Weighted Likeability')
-                )
-                .filter_by(
-                    engagement_type=EngagementType.Like,
-                )
-                .group_by(Engagement.content_id)
-                .order_by(func.sum((Engagement.engagement_value) * (315360000 - func.timestampdiff(text("SECOND"), Engagement.created_date, func.now()))).desc())
-                .limit(limit)
-                .offset(offset)
-                .all()
+        results = (
+            Engagement.query.with_entities(
+                Engagement.content_id, func.sum((Engagement.engagement_value) * (315360000 - func.timestampdiff(text("SECOND"), Engagement.created_date, func.now()))).label('Weighted Likeability')
             )
-            return list(map(lambda x: x[0], results))[:725], ([0]*len(results))[:725]
-        elif starting_point.get("content_id", False):
-            content_ids, scores = ann_with_offset(
-                starting_point["content_id"], 0.9, limit, offset, return_distances=True
+            .filter_by(
+                engagement_type=EngagementType.Like,
             )
-            return content_ids, scores
+            .group_by(Engagement.content_id)
+            .order_by(func.sum((Engagement.engagement_value) * (315360000 - func.timestampdiff(text("SECOND"), Engagement.created_date, func.now()))).desc())
+            .limit(limit)
+            .offset(offset)
+            .all()
+        )
+        return list(map(lambda x: x[0], results))[:725], ([0]*len(results))[:725]
     
     def _get_name(self):
         return "YourChoiceGenerator"
