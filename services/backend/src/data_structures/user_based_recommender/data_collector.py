7acd3930ef65a327c8deb999326af3c0f950378e
Kenneth Goodman <kennethgoodman@users.noreply.github.com>
2023-10-16 20:50:31 -0500

    trying something to make things more efficient

--- services/backend/src/data_structures/user_based_recommender/data_collector.py
+++ services/backend/src/data_structures/user_based_recommender/data_collector.py
@@ -3,6 +3,9 @@ from src import db
 import pandas as pd
 from src.api.engagement.models import Engagement
 import copy
+from sqlalchemy import or_, and_, func
+from sqlalchemy.sql.expression import bindparam
+
 
 class DataCollector:
     _instance = None  # Singleton instance reference
@@ -13,11 +16,26 @@ class DataCollector:
         return cls._instance
 
     def gather_data(self):
-    	self.result = db.session.query(
-            Engagement.content_id,
-            Engagement.user_id,
-            Engagement.engagement_type,
-            Engagement.engagement_value
+        self.result = (
+            session.query(
+                Engagement.content_id,
+                Engagement.user_id,
+                Engagement.engagement_type,
+                Engagement.engagement_value
+            )
+            .filter(
+                Engagement.user_id >= 77 # first user of Fall 2023
+            )
+            .from_self()
+            .add_columns(
+                over(
+                    func.row_number(),
+                    partition_by=Engagement.user_id,
+                    order_by=text("(RAND())")
+                ).label('rn')
+            )
+            .filter(text("rn <= 2000")) # get a max of 2k records per user
+            .order_by(Engagement.user_id, text("rn"))
         ).all()
 
     def get_data(self):
