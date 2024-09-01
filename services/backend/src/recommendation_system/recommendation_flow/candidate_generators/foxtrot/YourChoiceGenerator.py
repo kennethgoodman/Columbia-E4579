77d2fd0c0fbc3c644e22761206f2e5980271afeb
Kenneth Goodman <kennethgoodman@Kenneths-MacBook-Pro-3.local>
2024-08-31 21:31:59 -0400

    Removing fall 2023

--- services/backend/src/recommendation_system/recommendation_flow/candidate_generators/foxtrot/YourChoiceGenerator.py
+++
@@ -1,9 +0,0 @@
-
-from src.recommendation_system.recommendation_flow.candidate_generators.AbstractGenerator import AbstractGenerator
-
-class YourChoiceGenerator(AbstractGenerator):
-    def _get_content_ids(self, _, limit, offset, _seed, starting_point):
-        return [], []
-    
-    def _get_name(self):
-        return "YourChoiceGenerator"
--- services/backend/src/recommendation_system/recommendation_flow/controllers/fall_2023/__init__.py
+++
@@ -1,7 +0,0 @@
-from .AlphaController import AlphaController
-from .BetaController import BetaController
-from .CharlieController import CharlieController
-from .DeltaController import DeltaController
-from .EchoController import EchoController
-from .FoxtrotController import FoxtrotController
-from .GolfController import GolfController
